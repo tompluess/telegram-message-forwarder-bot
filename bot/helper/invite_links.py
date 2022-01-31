5a0856c239eabeec3901e121acc47b318d89aa61
Tom Plüss <tom.pluess@simplificator.com>
2022-01-31 19:38:27 +0100

    Make INVITE_LINKS_EXPIRE_DAYS configurable

--- bot/helper/invite_links.py
+++ bot/helper/invite_links.py
@@ -7,11 +7,13 @@ LOG.setLevel(environ.get("LOG_LEVEL", "INFO").upper())
 
 
 def get_invite_link(chat_id, app):
-    now = datetime.now()
-    expire_date = now + timedelta(days=7)
-    link_name = f"Message Forwarder Bot {now}"
-    LOG.debug(f"Link Name: {link_name}")
     try:
+        expire_days = int(environ.get("INVITE_LINKS_EXPIRE_DAYS", "7"))
+        now = datetime.now()
+        expire_date = now + timedelta(days=expire_days)
+        link_name = f"Message Forwarder Bot {now}"
+        LOG.debug(f"Link Name: {link_name}")
+
         invite_link = app.create_chat_invite_link(
             chat_id, name=link_name, creates_join_request=True, expire_date=int(expire_date.timestamp()))
         LOG.debug(f"got invite link for chat {chat_id}: {invite_link}")
