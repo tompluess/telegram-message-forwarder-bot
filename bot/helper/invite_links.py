2be63442c493f22dddc560ba125bfd1702aaf07a
Tom Plüss <tom.pluess@simplificator.com>
2022-01-31 16:12:49 +0100

    Invite Links create join requests

--- bot/helper/invite_links.py
+++ bot/helper/invite_links.py
@@ -5,10 +5,10 @@ LOG = logging.getLogger(__name__)
 
 
 def get_invite_link(chat_id, app):
-    expire_date = datetime.now() + timedelta(days=1)
+    expire_date = datetime.now() + timedelta(days=7)
     try:
         invite_link = app.create_chat_invite_link(
-            chat_id, name="Test Phase", expire_date=int(expire_date.timestamp()))
+            chat_id, name="Message Forwarder Bot", creates_join_request=True, expire_date=int(expire_date.timestamp()))
     except Exception as e:
         LOG.info(e)
         return None
