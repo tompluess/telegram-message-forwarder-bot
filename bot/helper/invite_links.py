9c275cbd4007aa0f39a776c08f5bfb22f5338bf4
Tom Plüss <tom.pluess@simplificator.com>
2022-02-11 14:14:16 +0100

    Cache invite links for 24h

--- bot/helper/invite_links.py
+++ bot/helper/invite_links.py
@@ -1,12 +1,27 @@
 import logging
 from os import environ
 from datetime import datetime, timedelta
+from cachetools import TTLCache
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(environ.get("LOG_LEVEL", "INFO").upper())
 
+invite_links_cache = TTLCache(maxsize=99, ttl=24*3600)
+
 
 def get_invite_link(chat_id, app):
+    try:
+        invite_link_from_cache = invite_links_cache[chat_id]
+        LOG.debug(f"using invite link from cache: {invite_link_from_cache}")
+        return invite_link_from_cache
+    except KeyError:
+        new_invite_link = create_invite_link(chat_id, app)
+        if new_invite_link:
+            invite_links_cache[chat_id] = new_invite_link
+        return new_invite_link
+
+
+def create_invite_link(chat_id, app):
     try:
         expire_days = int(environ.get("INVITE_LINKS_EXPIRE_DAYS", "7"))
         now = datetime.now()
@@ -21,3 +36,8 @@ def get_invite_link(chat_id, app):
     except Exception as e:
         LOG.info(e)
         return None
+
+
+def reset_invite_links():
+    LOG.warning("reset invite links")
+    invite_links_cache.clear()
--- bot/helper/invite_links_test.py
+++ bot/helper/invite_links_test.py
@@ -2,41 +2,69 @@ import logging
 from pyrogram.types import (
     InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ChatInviteLink)
 from pyrogram.types import (User, Message, Chat)
-from invite_links import get_invite_link
+from invite_links import get_invite_link, reset_invite_links
 
 LOG = logging.getLogger(__name__)
 
 
 def test_get_invite_link():
+    reset_invite_links()
     app = TestApp()
     # act
     invite_link = get_invite_link(100, app)
     # assert
-    assert invite_link == TestChatInviteLink.invite_link
+    assert invite_link == ChatInviteLinkFixture.invite_link_base+"1"
 
 
 def test_get_invite_link_no_permission():
-    app = TestAppNoPermission()
+    reset_invite_links()
+    app = AppNoPermissionFixture()
     # act
     invite_link = get_invite_link(100, app)
     # assert
     assert invite_link == None
 
 
+def test_get_invite_link_twice():
+    reset_invite_links()
+    app = TestApp()
+    # act
+    invite_link = get_invite_link(100, app)
+    invite_link = get_invite_link(100, app)
+    # assert
+    assert invite_link == ChatInviteLinkFixture.invite_link_base+"1"
+
+def test_get_invite_link_twice_different_chats():
+    reset_invite_links()
+    app = TestApp()
+    # act
+    invite_link = get_invite_link(100, app)
+    invite_link = get_invite_link(200, app)
+    # assert
+    assert invite_link == ChatInviteLinkFixture.invite_link_base+"2"
+
+
+
 class TestApp:
+    count = 0
+
     def create_chat_invite_link(self, chat_id, creates_join_request=False, name="Test", expire_date=None):
         assert chat_id, "chat id mandatory"
         assert creates_join_request, "join request mandatory"
         #LOG.debug(f"link name: {name}")
         if expire_date:
             assert isinstance(expire_date, int)
-        return TestChatInviteLink()
+        self.count = self.count+1
+        return ChatInviteLinkFixture(self.count)
 
 
-class TestChatInviteLink:
-    invite_link = "https://test_invite_link"
+class ChatInviteLinkFixture:
+    invite_link_base = "https://test_invite_link"
+
+    def __init__(self, id):
+        self.invite_link = self.invite_link_base+str(id)
 
 
-class TestAppNoPermission:
+class AppNoPermissionFixture:
     def create_chat_invite_link(self, chat_id, name="Test", expire_date=None):
         raise RuntimeError('simulated no permission exception')
